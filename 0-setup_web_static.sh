67970766f9cf1ccfc32968c5aa9e3ad64f4c02b6
EnockCity <enockmwanzia17@gmail.com>
2024-07-11 23:10:27 +0300

    deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
-# Script that sets up my servers for deployment
-
+# script that sets up your web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p '/data/web_static/releases/test'
-sudo mkdir -p /data/web_static/shared
-sudo ln -sf '/data/web_static/releases/test/' /data/web_static/current
-sudo touch /data/web_static/releases/test/index.html
-
-sudo chown -R ubuntu:ubuntu /data/
-
-new_string="server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}"
-sudo sed -i "s/server_name _;/$new_string/" /etc/nginx/sites-enabled/default
-
-echo -e "<html>\n  <head>\n  </head>\n    <body>\n      Hello World!\n    </body>\n</html>" | sudo tee '/data/web_static/releases/test/index.html'
-
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo chown -R ubuntu /data/web_static/releases/test
+echo "Holberton School" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+configure="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i "49i\ $configure" /etc/nginx/sites-available/default
 sudo service nginx restart
-
-
+sudo service nginx reload
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
+# Fabric to generate a .tgz archive from the contents of web_static.
+import os.path
+from datetime import datetime
+from fabric.api import local
 
-import datetime
-import fabric.api
-from fabric import task
-import os
 
-@task
-def do_pack:
-    if os.path.exists('web_static'):
-        now = datetime.datetime.now()
-        now = strftime('%Y%m%d%H%M%S')
-        filename = "web_static_" + now + ".tgz"
-        if not os.path.exists('versions'):
-            fabric.api.local("mkdir versions")
-        archive_path = "versions/" + filename
-        fabric.api.local(f"tar -cvzf {archive_path} web_static")
-        return archive_path
-    else:
+def do_pack():
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
+    return file
