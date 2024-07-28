d00639b00f5b7d4c7110c5a9dd2de00b09d7e8a9
EnockCity <enockmwanzia17@gmail.com>
2024-07-29 01:31:39 +0300

    flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-""" Flask framework
-    """
+"""
+This is a basic flask application
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/airbnb-onepage/", strict_slashes=False)
-def hello_world():
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
     return "Hello HBNB!"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
